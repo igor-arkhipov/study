14c14b9275837bd536e1ab1d535213d71f848e1c
Igor Arkhipov <igor.arkhipov@outlook.com>
2022-10-05 13:13:59 +0300

    Add solution for custom methods (fp2)

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,33 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+
+      def my_each(&block)
+        return self if size.zero?
+
+        first, *rest = self
+        yield first
+        MyArray.new(rest).my_each(&block)
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
-
-      # Написать свою функцию my_compact
-      def my_compact; end
+      def my_map
+        result = MyArray.new
+        my_each { |el| result.push(yield(el)) }
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(accumulator = nil)
+        my_each { |item| accumulator = accumulator.nil? ? item : yield(accumulator, item) }
+        accumulator
+      end
+
+      # Написать свою функцию my_compact
+      def my_compact
+        my_reduce(MyArray.new) { |accumulator, element| element.nil? ? accumulator : accumulator << element }
+      end
     end
   end
 end
